f3c970e6f1c1e96667353b84b1aebffd81fa88eb
Kevin Mason <krmason2020@gmail.com>
2022-10-16 12:33:15 -0700

    header looking good

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,10 @@
 const rootElement = document.body;
 // default style dark mode -- see theme.css
 // rootElement.classList.toggle('lightMode'); -- how to change theme
+
+//focus slider on label click
+document
+  .getElementById('tri-state-mode-display')
+  .addEventListener('click', () => {
+    document.getElementById('tri-state-slider').focus();
+  });
